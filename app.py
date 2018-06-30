ebd98f9233060e3b8b26f36936f32039075d0d78
yarving <yarving@gmail.com>
2018-06-30 14:23:56 +0800

    draft framework

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, request
+import requests
+from dateutil import parser, tz
+from twilio.twiml.messaging_response import MessagingResponse
+
+
+app = Flask(__name__)
+to_zone = tz.gettz('Asia/Shanghai')
+
+
+@app.route('/', methods=['POST'])
+def receive_sms():
+    body = requests.values.get('Body', None)
+    resp = MessagingResponse()
+    resp.message(body or 'Hello World!')
+    return str(resp)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
